469c00b7b354b07ff795bbd6e5b46b66beedfbb3
Christopher Canisius <canisius.christopher@googlemail.com>
2016-08-01 12:36:12 +0200

    Marked "public/private" methods, fixed destroy function

--- src/js/FloatingFormLabels.js
+++ src/js/FloatingFormLabels.js
@@ -22,49 +22,46 @@
         };
 
     function FloatingFormLabels (element, options) {
-        this.element = element;
-        this.options = $.extend({}, defaults, options);
-        this._defaults = defaults;
         this._name = pluginName;
-        this.el = $(this.element);
+        this.el = $(element);
+        this.options = $.extend({}, defaults, options);
         this.label = this.el.find(this.options.label);
         this.input = this.el.find(this.options.formElements);
-        this.init();
+        this._init();
     }
 
     FloatingFormLabels.prototype = {
-        init: function () {
+        _init: function () {
             var scope = this;
+            this._toggleClass(this._isFloated());
 
-            this.toggleClass(this.isFloated());
-
-            if (this.hasPlaceholder()) {
-                this.toggleClass(true);
+            if (this._hasPlaceholder()) {
+                this._toggleClass(true);
                 return;
             }
 
             this.input.on({
                 'focus.ffl': function () {
-                    scope.toggleClass(true);
+                    scope._toggleClass(true);
                 },
                 'blur.ffl': function () {
-                    scope.toggleClass(scope.isFloated());
+                    scope._toggleClass(scope._isFloated());
                 }
             });
         },
-        hasPlaceholder: function () {
+        _hasPlaceholder: function () {
             if (typeof this.input.attr('placeholder') !== 'undefined') {
                 return true;
             }
             return false;
         },
-        isFloated: function () {
+        _isFloated: function () {
             if (this.input.val() === '') {
                 return false;
             }
             return true;
         },
-        toggleClass: function (floated) {
+        _toggleClass: function (floated) {
             if (floated) {
                 this.label.addClass(this.options.floatedClass);
                 return;
@@ -74,16 +71,23 @@
         destroy: function () {
             this.input.off('focus.ffl, blur.ffl');
             this.label.removeClass(this.options.floatedClass);
-            delete $.fn[pluginName];
+
+            this.el.removeData(pluginName);
         }
     };
 
     $.fn[pluginName] = function (options) {
-        this.each(function () {
+        var instance,
+            args = Array.prototype.slice.call(arguments, 1);
+
+        return this.each(function () {
             if (!$.data(this, pluginName)) {
                 $.data(this, pluginName, new FloatingFormLabels(this, options));
+            } else if (typeof options === 'string') {
+                // call public methods with its own parameters
+                instance = $.data(this, pluginName);
+                instance[options].apply(instance, args);
             }
         });
-        return this;
     };
 }));
