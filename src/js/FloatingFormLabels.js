cfe905638199aa5ded00e777033025aba75e372f
Christopher Canisius <canisius.christopher@googlemail.com>
2016-08-01 10:35:13 +0200

    Removed unused wrapping function

--- src/js/FloatingFormLabels.js
+++ src/js/FloatingFormLabels.js
@@ -19,67 +19,64 @@
             label: '.ffl-label',
             formElements: 'input, textarea',
             floatedClass: 'ffl-floated'
-        },
-        FloatingFormLabels = (function () {
-            function Plugin (element, options) {
-                this.element = element;
-                this.options = $.extend({}, defaults, options);
-                this._defaults = defaults;
-                this._name = pluginName;
-                this.el = $(this.element);
-                this.label = this.el.find(this.options.label);
-                this.input = this.el.find(this.options.formElements);
-                this.init();
+        };
+
+    function FloatingFormLabels (element, options) {
+        this.element = element;
+        this.options = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.el = $(this.element);
+        this.label = this.el.find(this.options.label);
+        this.input = this.el.find(this.options.formElements);
+        this.init();
+    }
+
+    FloatingFormLabels.prototype = {
+        init: function () {
+            var scope = this;
+
+            this.toggleClass(this.isFloated());
+
+            if (this.hasPlaceholder()) {
+                this.toggleClass(true);
+                return;
             }
 
-            Plugin.prototype = {
-                init: function () {
-                    var scope = this;
-
-                    this.toggleClass(this.isFloated());
-
-                    if (this.hasPlaceholder()) {
-                        this.toggleClass(true);
-                        return;
-                    }
-
-                    this.input.on({
-                        'focus.ffl': function () {
-                            scope.toggleClass(true);
-                        },
-                        'blur.ffl': function () {
-                            scope.toggleClass(scope.isFloated());
-                        }
-                    });
+            this.input.on({
+                'focus.ffl': function () {
+                    scope.toggleClass(true);
                 },
-                hasPlaceholder: function () {
-                    if (typeof this.input.attr('placeholder') !== 'undefined') {
-                        return true;
-                    }
-                    return false;
-                },
-                isFloated: function () {
-                    if (this.input.val() === '') {
-                        return false;
-                    }
-                    return true;
-                },
-                toggleClass: function (floated) {
-                    if (floated) {
-                        this.label.addClass(this.options.floatedClass);
-                        return;
-                    }
-                    this.label.removeClass(this.options.floatedClass);
-                },
-                destroy: function () {
-                    this.input.off('focus.ffl, blur.ffl');
-                    this.label.removeClass(this.options.floatedClass);
-                    delete $.fn[pluginName];
+                'blur.ffl': function () {
+                    scope.toggleClass(scope.isFloated());
                 }
-            };
-
-            return Plugin;
-        }());
+            });
+        },
+        hasPlaceholder: function () {
+            if (typeof this.input.attr('placeholder') !== 'undefined') {
+                return true;
+            }
+            return false;
+        },
+        isFloated: function () {
+            if (this.input.val() === '') {
+                return false;
+            }
+            return true;
+        },
+        toggleClass: function (floated) {
+            if (floated) {
+                this.label.addClass(this.options.floatedClass);
+                return;
+            }
+            this.label.removeClass(this.options.floatedClass);
+        },
+        destroy: function () {
+            this.input.off('focus.ffl, blur.ffl');
+            this.label.removeClass(this.options.floatedClass);
+            delete $.fn[pluginName];
+        }
+    };
 
     $.fn[pluginName] = function (options) {
         this.each(function () {
